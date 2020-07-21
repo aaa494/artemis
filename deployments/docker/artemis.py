b048d5242dc683c51dffa662bc1cec513d7f1974
aidar <aidar@MacBook-Pro-Aidarbek.local>
2020-07-20 19:37:40 -0500

    changed artemis.py

--- deployments/docker/artemis.py
+++ deployments/docker/artemis.py
@@ -3,6 +3,6 @@ from flask import render_template
 app = Flask(_name_)
 @app.route('/')
 def index():
-    return 'Welcome to Artemis E-commerse web application.'
+    return 'Example code from dev-feature/example'
 if _name_ == '_main_':
     app.run(debug=True, host='0.0.0.0')
